0cc3007e8f70e23af9de713162c820f0b9b645a3
damour-dev <jeandamour876@gmail.com>
2025-05-08 10:53:10 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<?php 
+ session_start();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		body{
+			background-image: url(a2.jpg);
+			background-size: 100% 100% 100% 100%;
+		}
+		img{
+			height: 100px;
+			width: 100px;
+		}
+		
+
+		table{
+
+		}
+		td{
+			background-color: #202020;
+			color: white;
+			width: 150px;
+			opacity: .8;
+			font-family: Baskerville Old Face;
+			
+
+
+		}
+		#menu{
+			margin-top: 6%;
+
+		}
+			p{
+			font-family: Modern No. 20;
+			font-size: 24px;
+			color: white;
+		}
+		td:hover{
+			cursor: pointer;
+			box-shadow: 2px 3px 2px 3px;
+			font-size: 27px;
+			
+		}
+
+	</style>
+	<title>motorvehicles plates registering system</title>
+	<script type="text/javascript">window.history.forward();</script>
+</head>
+<body>
+	<br>
+	<center><p style="font-size: 40px;color: black;"><strong>MOTOR VEHICLE NUMBER PLATE</strong></p></center>
+	<center><p style="font-size: 40px;color: black;"><strong>REGISTRATION SYSTEM</strong></p></center>
+<center>
+	<div id="menu">
+	<table cellspacing="15">
+		<tr>
+			<td style="height: 150px;"><center><a href="adminlogin.php" style="text-decoration: none;"><img src="b (2).png"><br><p style="font-size: 13px;font-family: Lucida Fax;">ADMIN LOGIN</p></a></center></td>
+
+			<td style="height: 150px;"><center><a href="userlogin.php" style="text-decoration: none;"><img src="b (1).png"><br><p style="font-size: 13px;font-family: Lucida Fax;"> USER LOGIN</p></a></center></td>
+
+			<td style="height: 150px;"><center><a href="aboutus.php" style="text-decoration: none;"><img src="about.png" ><br><p style="font-size: 13px;font-family: Lucida Fax;">ABOUT US</p></a></center></td>
+		</tr>
+		<tr>
+			<td colspan="2" style="height: 50px;"><a href="howitworks.php" style="text-decoration: none;"><center><p style="font-size: 13px;font-family: Lucida Fax;">HOW THOSE THE SYSTEM WORK</p></center></td></a>
+			<td><a href="contactus.php" style="text-decoration: none;"><center><p style="font-size: 13px;font-family: Lucida Fax;">CONTACT US</p></center></a></td>
+		</tr>
+	</table>	
+	</div></center>
+
+</body>
+</html>
